cb9e04fb4fc655ec3683151a85015e24826cf395
Todsavad Tangtortan <guntsvzz@gmail.com>
2023-03-02 00:34:39 +0700

    update

---
+++ Course/Natural Language Understanding/Assignment/07 - Machine Translation TH-EN/NMTAttention_model.py
@@ -0,0 +1,223 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import random
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, emb_dim, hid_dim, dropout):
+        super().__init__()
+
+        self.embedding = nn.Embedding(input_dim, emb_dim)
+        self.rnn       = nn.GRU(emb_dim, hid_dim, bidirectional=True)
+        self.fc        = nn.Linear(hid_dim * 2, hid_dim)
+        self.dropout   = nn.Dropout(dropout)
+        
+    def forward(self, src, src_len):
+        
+        #src: [src len, batch size]
+        #src len: [batch size]
+        
+        embedded = self.dropout(self.embedding(src))
+        
+        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, 
+                            src_len.to('cpu'), enforce_sorted=False)
+        
+        #packed_outputs contain all hidden states including padding guy
+        #hidden contains the last hidden states of the non-padded guys
+        packed_outputs, hidden = self.rnn(packed_embedded)
+        #hidden: [n layers * num directions, batch size, hid dim]
+        
+        #convert packed_outputs to the guy that does not contain hidden states for padding
+        outputs, _ = nn.utils.rnn.pad_packed_sequence(packed_outputs)
+        #outputs: [src len, batch size, hid dim * num directions]
+        
+        #take the last hidden states from backward and forward
+        #hidden: (f, b, f, b)
+        forward  = hidden[-2, :, :]  #[batch size, hid dim]
+        backward = hidden[-1, :, :]  #[batch size, hid dim]
+        
+        hidden = torch.tanh(self.fc(torch.cat((forward, backward), dim = 1))) 
+        #hidden: [batch size, hid dim]
+        
+        return outputs, hidden
+
+class Attention(nn.Module):
+    def __init__(self, hid_dim, variants):
+        super().__init__()
+        self.variants = variants
+        self.v = nn.Linear(hid_dim, 1, bias = False)
+        self.W = nn.Linear(hid_dim,     hid_dim) #for decoder
+        self.U = nn.Linear(hid_dim * 2, hid_dim) #for encoder outputs
+                
+    def forward(self, hidden, encoder_outputs, mask):
+        
+        #hidden = [batch size, hid dim]
+        #encoder_outputs = [src len, batch size, hid dim * 2]
+        
+        batch_size = encoder_outputs.shape[1]
+        src_len = encoder_outputs.shape[0]
+
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        #encoder_outputs = [batch size, src len, hid dim * 2]
+
+        if self.variants == 'additive': #work
+            #repeat decoder hidden state src_len times
+            hidden = hidden.unsqueeze(1).repeat(1, src_len, 1)
+            #hidden = [batch size, src len, hid dim]
+            
+            energy = torch.tanh(self.W(hidden) + self.U(encoder_outputs))
+            #energy = [batch size, src len, hid dim]
+            
+            attention = self.v(energy).squeeze(2)
+            #attention = [batch size, src len]
+            
+        elif self.variants == 'general': #work
+            hidden = hidden.unsqueeze(1).repeat(1, 1, 2)
+            #hidden = [batch size, 1, hid dim*2]
+            #encoder_outputs = [batch size, hid dim * 2, src len]
+
+            energy = torch.bmm(hidden, encoder_outputs.transpose(1, 2))
+            attention = energy.squeeze(1)
+            #attention = [batch size, src len]
+
+        elif self.variants == 'multiplicative':
+            wh = self.W(hidden).unsqueeze(1).repeat(1, 1, 2)
+            #wh = [batch size, 1, hid dim*2]
+            #encoder_outputs = [batch size, hid dim * 2, src len]
+
+            energy = torch.bmm(wh, encoder_outputs.transpose(1, 2))
+            attention = energy.squeeze(1)
+
+        #use masked_fill_ if you want in-place
+        attention = attention.masked_fill(mask, -1e10)
+        #attention = [batch size, src len]
+        return F.softmax(attention, dim = 1)
+    
+class Decoder(nn.Module):
+    def __init__(self, output_dim, emb_dim, hid_dim, dropout, attention):
+        super().__init__()
+        
+        self.output_dim = output_dim
+        self.attention = attention
+        self.embedding = nn.Embedding(output_dim, emb_dim)
+        self.gru = nn.GRU((hid_dim * 2) + emb_dim, hid_dim)
+        self.fc = nn.Linear((hid_dim * 2) + hid_dim + emb_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, input, hidden, encoder_outputs, mask):
+        
+        #input = [batch size]
+        #hidden = [batch size, hid dim]
+        #encoder_outputs = [src len, batch size, hid dim * 2]
+        #mask = [batch size, src len]
+        
+        input = input.unsqueeze(0)
+        #input = [1, batch size]
+        
+        embedded = self.dropout(self.embedding(input))
+        #embedded = [1, batch size, emb dim]
+        
+        a = self.attention(hidden, encoder_outputs, mask)
+        #a = [batch size, src len]
+
+        a = a.unsqueeze(1)
+        #a = [batch size, 1, src len]
+        
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        #encoder_outputs = [batch size, src len, hid dim * 2]
+        
+        weighted = torch.bmm(a, encoder_outputs) #Ct
+        #weighted = [batch size, 1, hid dim * 2]
+        
+        weighted = weighted.permute(1, 0, 2)
+        #weighted = [1, batch size, hid dim * 2]
+        
+        rnn_input = torch.cat((embedded, weighted), dim = 2)
+        #rnn_input = [1, batch size, (hid dim * 2) + emb dim]
+        
+        output, hidden = self.gru(rnn_input, hidden.unsqueeze(0))
+        #output = [seq len, batch size, dec hid dim * n directions]
+        #hidden = [1, batch size, hid dim]
+        assert (output == hidden).all()
+
+        embedded = embedded.squeeze(0)
+        output   = output.squeeze(0)
+        weighted = weighted.squeeze(0)
+        
+        prediction = self.fc(torch.cat((output, weighted, embedded), dim = 1))
+        #prediction = [batch size, output dim]
+        
+        return prediction, hidden.squeeze(0), a.squeeze(1)
+    
+
+class Seq2SeqPackedAttention(nn.Module):
+    def __init__(self, encoder, decoder, src_pad_idx, device):
+        super().__init__()
+        
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_pad_idx = src_pad_idx
+        self.device = device
+        
+    def create_mask(self, src):
+        #src: [src len, batch size]
+        
+        mask = (src == self.src_pad_idx).permute(1, 0)
+        #mask: [batch size, src len]
+        #we need to permute to make the mask same shape as attention...
+        return mask
+
+        
+    def forward(self, src, src_len, trg, teacher_forcing_ratio = 0.5):
+        
+        #src: [src len, batch size]
+        #src len: [batch size]
+        #trg: [trg len, batch size]
+        
+        batch_size = src.shape[1]
+        trg_len    = trg.shape[0]
+        trg_vocab_size = self.decoder.output_dim #define in decoder
+        
+        #because decoder decodes each step....let's create a list that gonna append the result to this guy
+        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(self.device)
+        
+        #because decoder decodes each step....let's memorize the attention done in each step....
+        attentions = torch.zeros(trg_len, batch_size, src.shape[0]).to(self.device)
+        
+        #let's start!!!
+        #1. encoder
+        encoder_outputs, hidden = self.encoder(src, src_len)
+        #encoder_outputs: [src len, batch size, hid dim * num directions]
+        #hidden: [batch size, hid dim]
+        
+        #set the first input to the decoder
+        input_ = trg[0,:]  #basically <sos>
+        
+        #create the mask for use in this step
+        mask = self.create_mask(src)
+        
+        #2. for each of trg word
+        for t in range(1, trg_len):
+
+            #3. decode (hidden is always carry forward)
+            output, hidden, attention = self.decoder(input_, hidden, encoder_outputs, mask)
+            #output:   [batch size, output_dim]
+            #hidden:   [batch size, hid_dim]
+            #attention::[batch size, src len]  ==> how each of src token is important to input_ 
+            
+            #4. append the results to outputs and attentions
+            outputs[t] = output
+            attentions[t] = attention
+            
+            #5. get the result, using argmax
+            top1 = output.argmax(1)  #find the maximum index of dimension 1, which is output_dim
+            
+            #6. based on the teacher forcing ratio, 
+            teacher_force_or_not = random.random() < teacher_forcing_ratio
+                    #if teacher forcing, next input is the next trg
+                    #if no teacher forcing, the next input is the argmax guy...
+            input_ = trg[t] if teacher_force_or_not else top1  #autoregressive
+            
+        return outputs, attentions #outputs for predicting the word, attentions to see which word is important
+        
+        
