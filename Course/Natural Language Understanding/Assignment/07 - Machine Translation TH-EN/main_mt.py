14e7cd5da036ef951cff03da7e1ebb18aad088d0
Todsavad Tangtortan <guntsvzz@gmail.com>
2023-03-02 16:27:27 +0700

    update

--- Course/Natural Language Understanding/Assignment/07 - Machine Translation TH-EN/main_mt.py
+++ Course/Natural Language Understanding/Assignment/07 - Machine Translation TH-EN/main_mt.py
@@ -1,5 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from wtforms.validators import DataRequired
+from TestingNMT import *
+# import TestingNMT
+import torch
+
+import torch, torchdata, torchtext
+from torch import nn
+import torch.nn.functional as F
+
+import random, math, time
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+#make our work comparable if restarted the kernel
+SEED = 1234
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+variants = 'additive'
+save_path = './models/Seq2SeqPackedAttention_additive.pt'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mykey'
@@ -15,9 +33,14 @@ def about():
 
 @app.route('/mt', methods = ['GET','POST'])
 def machinetranslation():
-    source = request.args.get('source')
-    target = request.args.get('targets')
-    data = {"source":source,"target":target}
+    if request.method == 'POST':
+        source = request.form.get('source')
+        predict = translation(source, variants, save_path, device)
+    else:
+        source = ' '
+        predict = ' '
+    # predict = 555
+    data = {"source":source, "predict":predict}
     return render_template("mt.html", data = data)
 
 if __name__ == "__main__":
