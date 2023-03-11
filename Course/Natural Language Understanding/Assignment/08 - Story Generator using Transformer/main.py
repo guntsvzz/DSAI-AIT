2c44e3b882d2855ae03c13d4d2f72c9ad3a0952e
Todsavad Tangtortan <guntsvzz@gmail.com>
2023-03-11 08:13:42 +0700

    update

---
+++ Course/Natural Language Understanding/Assignment/08 - Story Generator using Transformer/main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField
+from wtforms.validators import DataRequired
+from werkzeug.utils import secure_filename
+from wtforms.validators import InputRequired
+import pandas as pd
+from Predict_Nextword import predict
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mykey'
+app.config['UPLOAD_FOLDER'] = 'static/files' 
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+class MyForm(FlaskForm):
+    name = StringField('Type something', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/story', methods = ['GET','POST'])
+def story():
+    form = MyForm()
+    code = False
+    name = False
+    print(form.validate_on_submit())
+    if form.validate_on_submit():
+        name = form.name.data 
+        code = predict(prompt = name, temperature=0.5)
+        form.name.data = ""
+    return render_template("autocomplete.html",form=form,name =name, code=code)
+
+if __name__ == "__main__":
+    app.run(debug=True)
