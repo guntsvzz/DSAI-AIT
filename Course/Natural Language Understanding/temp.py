694d33021fcc6eaac3a4b585126264a5401ab964
Todsavad Tangtortan <guntsvzz@gmail.com>
2023-05-17 13:02:54 +0700

    update

--- Course/Natural Language Understanding/temp.py
+++
@@ -1,8 +0,0 @@
-import timm, tome
-
-# Load a pretrained model, can be any vit / deit model.
-model = timm.create_model("vit_base_patch16_224", pretrained=True)
-# Patch the model with ToMe.
-tome.patch.timm(model)
-# Set the number of tokens reduced per layer. See paper for details.
-model.r = 16
